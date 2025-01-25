19d928af564d89f076b8e4a9340363f8716a9e70
Judith Jude <ifunanyajudith09@gmail.com>
2025-01-25 08:46:01 +0100

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,13 +3,15 @@ import CartList from '@/components/CartList';
 
 const Cart = () => {
   return (
-    <main>
-      <h1>cart</h1>
-      <ul>
+    <main className="py-8 px-6 relative ">
+      <h1 className="font-bold text-2xl mb-4 ">Desserts</h1>
+      <ul className="grid gap-4 lg:grid-cols-3 lg:pr-80 relative">
         {data.map((data, i) => (
           <CartList data={data} key={i} />
         ))}
       </ul>
+      <h2 className="block lg:hidden ">many men</h2>
+      <h2 className="absolute position hidden lg:block">many men</h2>
     </main>
   );
 };
--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,11 +1,34 @@
+import Button from '@/components/Button';
+import addToCart from '@/assets/images/icon-add-to-cart.svg';
 const CartList = ({ data }) => {
   return (
     <main>
-      <li>
-        <div>
-          <img src={data.image.desktop} alt={data.name} />
-          <img src={data.image.tablet} alt={data.name} />
-          <img src={data.image.mobile} alt={data.name} />
+      <li className="">
+        <div className="relative">
+          <img
+            src={data.image.mobile}
+            alt={data.name}
+            className="rounded-xl  lg:hidden md:hidden"
+          />
+          <img
+            src={data.image.tablet}
+            alt={data.name}
+            className="rounded-xl lg:hidden hidden md:block "
+          />
+          <img
+            src={data.image.desktop}
+            alt={data.name}
+            className="rounded-xl hidden md:hidden lg:block"
+          />
+          <Button className="font-semibold flex gap-2 border-Rose-500 border-2 rounded-3xl bg-Rose-50 px-4 py-2  absolute  ">
+            <img src={addToCart} alt="" />
+            <p className="self-center">Add to Cart</p>
+          </Button>
+        </div>
+        <div className="mt-12 mb-4 font-medium">
+          <p className="text-Rose-500 ">{data.name}</p>
+          <p className="text-xl font-semibold">{data.category}</p>
+          <p className="text-Red">${data.price}</p>
         </div>
       </li>
     </main>
